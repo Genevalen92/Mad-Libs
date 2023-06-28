75ff6887797806d67b4ad5c17fe830c60ec8db8e
Genevalen92 <59771843+Genevalen92@users.noreply.github.com>
2023-06-28 11:46:27 -0500

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,32 @@
+#greet the user
+
+print("Welcome to madlibs!")
+#Have user input the blanks for madlibs
+
+adjective = input("Add an adjective. \n")
+adjective_2 = input("Add another adjective. \n")
+bird = input("Add a bird type. \n")
+room = input("What type of room. \n")
+verb_past = input("Add a past tense verb. \n")
+verb_regular = input("Add a verb. \n")
+relative = input("Add a relative. \n")
+noun = input("Add a noun. \n")
+liquid = input("Add a liquid. \n")
+verb_ing = input("Add verb with an ing ending. \n")
+body_part = input("Add body part must be plural. \n")
+p_noun = input("Add a plural noun. \n")
+verb2_ing = input("Add another verb with an ing. \n")
+noun_2 = input("add another noun. \n")
+
+#Print out madlib with user answers
+
+print(f"It was a {adjective}, cold November day.")
+print(f"I woke up to the {adjective_2} smell of {bird} roasting in the {room} downstairs.")
+print(f"I {verb_past} down the stairs to see if I could help {verb_regular} the dinner.")
+print(f"My mom said, See if {relative} needs a fresh {noun}.")
+
+print(f"So I carried a tray of glasses full of {liquid} into the {verb_ing} room.")
+
+print(f"When I got there, I could not belive my {body_part}!")
+
+print(f"There were {p_noun} {verb2_ing} on the {noun_2}!")
